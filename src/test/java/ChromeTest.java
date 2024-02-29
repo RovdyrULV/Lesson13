2e8a779aadf65668c2d2361e7e519b0155a24011
Rovdyr_UlV <rovdyrulv@gmail.com>
2024-02-29 14:02:43 +0300

    1

--- src/test/java/ChromeTest.java
+++ src/test/java/ChromeTest.java
@@ -4,8 +4,6 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.openqa.selenium.support.ui.ExpectedCondition;
-import org.openqa.selenium.support.ui.ExpectedConditions;
 
 import java.util.concurrent.TimeUnit;
 
@@ -22,7 +20,6 @@ class ChromeTest {
     void setup() {
         driver = new ChromeDriver();
         driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.get("https://www.mts.by/");
         WebElement cookieaccept = driver.findElement(By.xpath("/html/body/div[6]/main/div/div[2]/div/div[2]/button[3]"));
         cookieaccept.click();
@@ -33,48 +30,90 @@ class ChromeTest {
     void teardown() {
         driver.quit();
     }
-    
-    @Test
-     void blockNameTest() {
-        WebElement onlinereplenishmentwithoutcommission = driver.findElement(By.xpath("/html/body/div[6]/main/div/div[4]/div[1]/div/div/div[2]/section/div/h2"));
-        String onlinereplenishmentwithoutcommissiontext = onlinereplenishmentwithoutcommission.getAttribute("textContent");
-        Assertions.assertEquals(onlinereplenishmentwithoutcommissiontext, "Онлайн пополнение без комиссии");
-        // Альтернатива?
-        blockNameCheck(ExpectedConditions.textToBePresentInElement(onlinereplenishmentwithoutcommission, "Онлайн пополнение без комиссии"));
-    }
-
-    private void blockNameCheck(ExpectedCondition<Boolean> text) {
-    }
 
     @Test
-    void availabilityOfPaymentSystems(){
-        WebElement paymentSystems = driver.findElement(By.xpath("//*[@id=\"pay-section\"]/div/div/div[2]/section/div/div[2]/ul"));
-        //WebElement paymentSystems = driver.findElement(By.xpath("/html/body/div[6]/main/div/div[3]/div[1]/div/div/div[2]/section/div/div[2]"));
-        var paylist = paymentSystems.getAttribute("children");
-        System.out.println(paylist);
-    }
-
-
-    @Test
-    void checkTheLinkMoreAboutTheService() {
-        WebElement linkAboutTheService = driver.findElement(By.xpath("/html/body/div[6]/main/div/div[4]/div[1]/div/div/div[2]/section/div/a"));
-        linkAboutTheService.click();
-        Assertions.assertEquals(driver.getCurrentUrl(),"https://www.mts.by/help/poryadok-oplaty-i-bezopasnost-internet-platezhey/");
-    }
-
-    @Test
-    void communicationServicesTest(){
+    void communicationServicesIframeTest() {
         WebElement enternumber = driver.findElement(By.xpath("//*[@id=\"connection-phone\"]"));
-        //WebElement enternumber = driver.findElement(By.xpath("/html/body/div[6]/main/div/div[3]/div[1]/div/div/div[2]/section/div/div[1]/div[2]/form[1]/div[1]/input"));
         enternumber.click();
         enternumber.sendKeys("297777777");
         WebElement entersum = driver.findElement(By.xpath("//*[@id=\"connection-sum\"]"));
-        //WebElement entersum = driver.findElement(By.xpath("/html/body/div[6]/main/div/div[3]/div[1]/div/div/div[2]/section/div/div[1]/div[2]/form[1]/div[2]/input"));
         entersum.click();
         entersum.sendKeys("50");
-        //WebElement resumebutton = driver.findElement(By.xpath("/html/body/div[6]/main/div/div[3]/div[1]/div/div/div[2]/section/div/div[1]/div[2]/form[1]/button"));
+        WebElement emailtosend = driver.findElement(By.xpath("//*[@id=\"connection-email\"]"));
+        emailtosend.click();
+        emailtosend.sendKeys("Ivanov@gmail.com");
         WebElement resumebutton = driver.findElement(By.xpath("//*[@id=\"pay-connection\"]/button"));
         resumebutton.click();
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        WebElement fr = driver.findElement(By.xpath("//iframe[@allowpaymentrequest][@style='visibility: visible;']"));
+        driver.switchTo().frame(fr);
+        WebElement paymentamount = driver.findElement(By.xpath("/html/body/app-root/div/div/app-payment-container/app-header/header/div/div/div"));
+        Assertions.assertEquals(paymentamount.getAttribute("textContent"), "50.00 BYN");
+        WebElement phonenumber = driver.findElement(By.xpath("/html/body/app-root/div/div/app-payment-container/app-header/header/div/div/p"));
+        Assertions.assertEquals(phonenumber.getAttribute("innerText"), "Оплата: Услуги связи Номер:375297777777");
+        WebElement inputcardnumber = driver.findElement(By.xpath("/html/body/app-root/div/div/app-payment-container/section/app-card-page/div/div[1]/app-card-input/form/div[1]/div[1]/app-input/div/div/div[1]/label"));
+        Assertions.assertEquals(inputcardnumber.getAttribute("innerText"), "Номер карты");
+        WebElement paymentsystems = driver.findElement(By.xpath("/html/body/app-root/div/div/app-payment-container/section/app-card-page/div/div[1]/app-card-input/form/div[1]/div[1]/app-input/div/div/div[2]/div"));
+        paymentsystems.getAttribute("children");
+        WebElement validity = driver.findElement(By.xpath("/html/body/app-root/div/div/app-payment-container/section/app-card-page/div/div[1]/app-card-input/form/div[1]/div[2]/div[1]/app-input/div/div/div[1]/label"));
+        Assertions.assertEquals(validity.getAttribute("innerText"), "Срок действия");
+        WebElement cvc = driver.findElement(By.xpath("/html/body/app-root/div/div/app-payment-container/section/app-card-page/div/div[1]/app-card-input/form/div[1]/div[2]/div[3]/app-input/div/div/div[1]/label"));
+        Assertions.assertEquals(cvc.getAttribute("innerText"), "CVC");
+        WebElement holdername = driver.findElement(By.xpath("/html/body/app-root/div/div/app-payment-container/section/app-card-page/div/div[1]/app-card-input/form/div[1]/div[3]/app-input/div/div/div[1]/label"));
+        Assertions.assertEquals(holdername.getAttribute("innerText"), "Имя держателя (как на карте)");
+        WebElement patbutton = driver.findElement(By.xpath("/html/body/app-root/div/div/app-payment-container/section/app-card-page/div/div[1]/button"));
+        Assertions.assertEquals(patbutton.getAttribute("innerText"), "Оплатить 50.00 BYN");
     }
-    //Xpath почему-то полный путь не работал, не знаю стоит ли еще добавлять Assertion.
+
+    @Test
+    void communicationServicesEmptyFieldsTest(){
+        WebElement enternumber = driver.findElement(By.xpath("//*[@id='connection-phone']"));
+        Assertions.assertEquals(enternumber.getAttribute("placeholder"), "Номер телефона");
+        WebElement entersum = driver.findElement(By.xpath("//*[@id=\"connection-sum\"]"));
+        Assertions.assertEquals(entersum.getAttribute("placeholder"), "Сумма");
+        WebElement emailtosend = driver.findElement(By.xpath("//*[@id=\"connection-email\"]"));
+        Assertions.assertEquals(emailtosend.getAttribute("placeholder"), "E-mail для отправки чека");
+    }
+
+    @Test
+    void homeInternetEmptyFieldsTest(){
+        WebElement selectlist = driver.findElement(By.xpath("//*[@id=\"pay-section\"]/div/div/div[2]/section/div/div[1]/div[1]/div[2]/button"));
+        selectlist.click();
+        WebElement homeinternet = driver.findElement(By.xpath("//*[@id=\"pay-section\"]/div/div/div[2]/section/div/div[1]/div[1]/div[2]/ul/li[2]/p"));
+        homeinternet.click();
+        WebElement subscribernumber = driver.findElement(By.xpath("//*[@id=\"internet-phone\"]"));
+        Assertions.assertEquals(subscribernumber.getAttribute("placeholder"), "Номер абонента");
+        WebElement entersum = driver.findElement(By.xpath("//*[@id=\"connection-sum\"]"));
+        Assertions.assertEquals(entersum.getAttribute("placeholder"), "Сумма");
+        WebElement emailtosend = driver.findElement(By.xpath("//*[@id=\"connection-email\"]"));
+        Assertions.assertEquals(emailtosend.getAttribute("placeholder"), "E-mail для отправки чека");
+    }
+
+    @Test
+    void installmentPlanTest() {
+        WebElement selectlist = driver.findElement(By.xpath("//*[@id=\"pay-section\"]/div/div/div[2]/section/div/div[1]/div[1]/div[2]/button"));
+        selectlist.click();
+        WebElement installmentplan = driver.findElement(By.xpath("//*[@id=\"pay-section\"]/div/div/div[2]/section/div/div[1]/div[1]/div[2]/ul/li[3]/p"));
+        installmentplan.click();
+        WebElement accountnumber = driver.findElement(By.xpath("//*[@id=\"score-instalment\"]"));
+        Assertions.assertEquals(accountnumber.getAttribute("placeholder"), "Номер счета на 44");
+        WebElement entersum = driver.findElement(By.xpath("//*[@id=\"connection-sum\"]"));
+        Assertions.assertEquals(entersum.getAttribute("placeholder"), "Сумма");
+        WebElement emailtosend = driver.findElement(By.xpath("//*[@id=\"connection-email\"]"));
+        Assertions.assertEquals(emailtosend.getAttribute("placeholder"), "E-mail для отправки чека");
+    }
+
+    @Test
+    void debtTest() {
+        WebElement selectlist = driver.findElement(By.xpath("//*[@id=\"pay-section\"]/div/div/div[2]/section/div/div[1]/div[1]/div[2]/button"));
+        selectlist.click();
+        WebElement debt = driver.findElement(By.xpath("//*[@id=\"pay-section\"]/div/div/div[2]/section/div/div[1]/div[1]/div[2]/ul/li[4]/p"));
+        debt.click();
+        WebElement accountnumber = driver.findElement(By.xpath("//*[@id=\"score-arrears\"]"));
+        Assertions.assertEquals(accountnumber.getAttribute("placeholder"), "Номер счета на 2073");
+        WebElement entersum = driver.findElement(By.xpath("//*[@id=\"connection-sum\"]"));
+        Assertions.assertEquals(entersum.getAttribute("placeholder"), "Сумма");
+        WebElement emailtosend = driver.findElement(By.xpath("//*[@id=\"connection-email\"]"));
+        Assertions.assertEquals(emailtosend.getAttribute("placeholder"), "E-mail для отправки чека");
+    }
 }
